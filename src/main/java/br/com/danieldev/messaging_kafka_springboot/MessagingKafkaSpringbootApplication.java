86cbeac21a0bdde713f2751763bb18d7eb29ed65
Daniel Lopes <viana.daniel02@gmail.com>
2025-05-24 15:00:34 -0300

    feat: adiciona docker-compose para Kafka e Spring Boot

---
+++ src/main/java/br/com/danieldev/messaging_kafka_springboot/MessagingKafkaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.danieldev.messaging_kafka_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingKafkaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingKafkaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danieldev/messaging_kafka_springboot/MessagingKafkaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danieldev.messaging_kafka_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingKafkaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
